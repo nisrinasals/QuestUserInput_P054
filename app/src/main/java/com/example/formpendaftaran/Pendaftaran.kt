9bfc1bc5183d87c4bade5762c392b34bdff8d86c
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-01 13:41:49 +0700

    menambahkan pilihan gender dengan menggunakan column

--- app/src/main/java/com/example/formpendaftaran/Pendaftaran.kt
+++ app/src/main/java/com/example/formpendaftaran/Pendaftaran.kt
@@ -4,14 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -20,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -88,6 +92,24 @@ fun FormPendaftaran(modifier : Modifier) {
                 Text(stringResource(R.string.jenis).uppercase(),
                     modifier = Modifier.padding(5.dp)
                 )
+                Column(
+                    modifier = Modifier.padding(5.dp)
+                ) {
+                    gender.forEach { item ->
+                        Row(
+                            modifier = Modifier.selectable(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            ), verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = textJK == item,
+                                onClick = {
+                                    textJK = item
+                                })
+                            Text(item)
+                        }
+                    }
+                }
 
             }
         }
