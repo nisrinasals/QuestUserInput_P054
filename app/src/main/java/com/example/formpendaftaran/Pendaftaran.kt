099dceee9b2263b4ab6c203f0b121a239a27a0fc
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-01 13:21:30 +0700

    menambahkan var yang digunakan

--- app/src/main/java/com/example/formpendaftaran/Pendaftaran.kt
+++ app/src/main/java/com/example/formpendaftaran/Pendaftaran.kt
@@ -1,10 +1,25 @@
 package com.example.formpendaftaran
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun FormPendaftaran(modifier : Modifier) {
+
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+    var nama by remember {mutableStateOf("")}
+    var alamat by remember {mutableStateOf("")}
+    var jenis by remember {mutableStateOf("")}
+    var status by remember {mutableStateOf("")}
+
 
 }
