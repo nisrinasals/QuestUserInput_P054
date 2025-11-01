dbe01eef562df69a49f4e5288b0f99d910031153
nisrina salsa <nisrinahikmawan@gmail.com>
2025-11-01 13:19:25 +0700

    membuat fun FormPendaftaran pada Pendaftaran.kt

---
+++ app/src/main/java/com/example/formpendaftaran/Pendaftaran.kt
@@ -0,0 +1,10 @@
+package com.example.formpendaftaran
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FormPendaftaran(modifier : Modifier) {
+
+}
